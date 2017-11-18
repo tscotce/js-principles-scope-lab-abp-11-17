c23b3faa60214399eb9061cfb9a095bcdcb1f8a8
tscotce <tscotce@gmail.com>
2017-11-18 17:49:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var customerName = 'bob'
 function upperCaseCustomerName (){
-  return customerName.toUpperCase ();
+  customerName.toUpperCase ();
 }
 function setBestCustomer(){
   bestCustomer = 'not bob'

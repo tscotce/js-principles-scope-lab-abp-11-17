d018f220dbc2238960477275efe476cb6457c3d2
tscotce <tscotce@gmail.com>
2017-11-18 17:21:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var customerName = 'bob'
-function upperCaseCustomerName (customerName) {
-  return customerName.toUpperCase ();
+function upperCaseCustomerName () {
+  return customerName.toUpperCase (customerName);
 }
